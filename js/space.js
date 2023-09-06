48f53a2c8e807b2c12988d8809291fb8529c4378
ValeBo46 <valebonilla1993@gmail.com>
2023-09-06 19:55:34 -0300

    clases

--- js/space.js
+++ js/space.js
@@ -11,22 +11,35 @@ function showData(data) {
     // A través de un forEach agregamos el contenido encontrado en el html
     data.collection.items.forEach(item => {
 
+        const div = document.createElement("div");
+        div.classList.add("card");
+
         const img = document.createElement("img");
         img.src = item.links[0].href;
-        contenedor.appendChild(img);
+        img.classList.add("card-img-top");
+        div.appendChild(img);
+
+        const body = document.createElement("div");
+        body.classList.add("card-body");
 
         const title = document.createElement("h2");
         title.textContent = item.data[0].title;
-        contenedor.appendChild(title);
+        title.classList.add("card-title");
+        div.appendChild(title);
 
         const description = document.createElement("p");
         description.textContent = item.data[0].description;
-        contenedor.appendChild(description);
+        description.classList.add("card-text");
+        div.appendChild(description);
+
+        const pie = document.createElement("div");
+        pie.classList.add("card-footer");
 
         const date = document.createElement("p");
         date.textContent = `Fecha de creación: ${item.data[0].date_created}`;
-        contenedor.appendChild(date);
-
+        date.classList.add("text-body-secondary");
+        div.appendChild(date);
+        contenedor.appendChild(div);
 
     });
 }
